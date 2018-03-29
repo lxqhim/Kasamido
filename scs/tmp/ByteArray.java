3dc16a10abf07f59c86504b98ef9ddf1f801507a
lxqhim <lxqhim@gmail.com>
2018-03-29 12:33:13 +0200

    adding class ByteArray

---
+++ scs/tmp/ByteArray.java
@@ -0,0 +1,17 @@
+public class ByteArray{
+    public static byte[] intToByteArray(int i) {
+        byte[] result = new byte[4];
+        // 由高位到低位
+        result[0] = (byte) ((i >> 24) & 0xFF);
+        result[1] = (byte) ((i >> 16) & 0xFF);
+        result[2] = (byte) ((i >> 8) & 0xFF);
+        result[3] = (byte) (i & 0xFF);
+        return result;
+    }
+
+    public static void init(byte [] b){
+        for(int i=0; i<b.length; i++){
+            b[i] = 0;
+        }
+    }
+}
